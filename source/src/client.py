77a3281cf40eecb7031752e6babff0a30a140382
AmarjotS82 <amarjot.sangha28@gmail.com>
2024-09-26 14:09:53 -0700

    Adding intital source code needs to be tested

---
+++ source/src/client.py
@@ -0,0 +1,88 @@
+import sys
+import socket
+
+def connect_to_server(port_num, ip_addr):
+    new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        new_socket.connect((ip_addr, port_num))
+    except ConnectionRefusedError:
+        sys.exit("Error: Connection refused server is not listening")
+    except FileNotFoundError:
+        sys.exit("Error: File not found, check that the server is listening and the socket path is the same")
+    return new_socket
+
+def send_request(file, connected_socket):
+    print("sending request...")
+    connected_socket.send(str.encode(file))
+
+def recieve_request(connected_socket):
+    print("recievieng request...")
+    try:
+        data = connected_socket.recv(1024)
+    except ConnectionResetError:
+        sys.exit("Error: Server disconnected")
+    except KeyboardInterrupt:
+        close_connection(connected_socket)
+        sys.exit("\nYou have disconnected from the server")
+    print(data.decode("utf-8"))
+
+def handle_arguments(args):
+    if len(args) == 1:
+        sys.exit("Error: no arguments provided need -p server port number -i server ip address -f file ")
+    
+    flags = ["-p", "-i", "-f"]
+    
+    if len(args) > 7:
+        sys.exit("Error: Too many arguments provided")
+    
+    for i in range(len(flags)):
+        try:
+            index = args.index(flags[i])
+            arg_label = ""
+            if i == 0:
+                arg_label += "port number"
+            elif i == 1:
+                arg_label += "ip address" 
+            else:
+                 arg_label += "file" 
+        except ValueError:
+            sys.exit("Error: need " + flags[i] + " flag before " + arg_label)
+
+        if len(args) == (index+1) or not args[(index+1)].strip():
+            sys.exit("Error: no " + arg_label + " provided")   
+    return args
+
+def parse_arguments(args):
+    flags = ["-p","-i","-f"]
+    parsed_values = []
+    for i in range(len(flags)):
+        index = args.index(flags[i])
+        parsed_value = args[index + 1]
+        # name_of_path = ""
+        # if i == 0:
+        #     name_of_path += "file"
+        # else:
+        #     name_of_path += "socket"
+        parsed_values.append(parsed_value)
+    return parsed_values 
+
+def close_connection(client_socket):
+    client_socket.close()
+
+def main():
+    cmd_args = sys.argv
+    validated_args = handle_arguments(cmd_args)
+    parsed_values = parse_arguments(validated_args)
+
+    port_num = int(parsed_values[0])
+    ip_address = parsed_values[1]
+    file = parsed_values[2]
+    print(parsed_values)
+    client_socket = connect_to_server(port_num, ip_address)
+    print("Connected to server on port number " + port_num + " at IP address " + ip_address)
+    send_request(file, client_socket)
+    recieve_request(client_socket)
+    close_connection(client_socket)
+
+
+main()
---
+++ source/src/server.py
@@ -0,0 +1,96 @@
+import socket
+import os
+import sys
+import time
+
+def handle_arguments(args):
+    if len(args) < 2:
+        sys.exit("Error: no arguments provided")
+    if len(args) > 3:
+        sys.exit("Error: Too many arguments provided")
+    if "-p" in args:
+        try:
+            index = args.index("-p")
+        except ValueError:
+            sys.exit("Error: need -p flag before port number")
+
+    
+    return args
+
+def parse_arguments(args):
+    parsed_args = []
+    index = args.index("-p")
+    if (index+1) != len(args):
+        port_num = args[index + 1]
+        parsed_args.append(int(port_num))
+    
+    name = socket.gethostname()
+    ip_address = socket.gethostbyname(name)
+    parsed_args.append(ip_address)
+    return parsed_args 
+
+def handle_client_connection(server_socket):
+    
+    conn, addr = server_socket.accept()
+    if conn:
+        print("accepted client connection...")
+    return conn
+
+def start_server(parsed_args):
+
+    port_num = parsed_args[0]
+    ip_address = parsed_args[1]
+
+    new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        new_socket.bind((ip_address, port_num))
+    except PermissionError:
+        sys.exit("Permision denied! Use another port number!")
+
+    while(True):
+        try:
+            new_socket.listen(10)
+            print("Server is listening on port number " + str(port_num) + " at IP address " + ip_address)
+            connection = handle_client_connection(new_socket)
+            recieved_data = connection.recv(1024)
+            decoded_data = recieved_data.decode("utf-8")
+            handle_client_request(decoded_data, connection)
+        except KeyboardInterrupt:
+            try:
+                try: 
+                    connection.send(str.encode("Error: Server disconnected"))
+                    connection.close()
+                except BrokenPipeError:
+                    sys.exit("\nServer disconnected")
+            except UnboundLocalError:
+                sys.exit("\nServer disconnected")
+            sys.exit("\nServer disconnected")
+        
+def count_letters_case_sensitive(file_content):
+    unique_letters = []
+    all_letters = list(file_content)
+    for letter in all_letters:
+        if letter not in unique_letters:
+            unique_letters.append(letter)
+    
+    number_of_unique_letters = len(unique_letters)
+    return number_of_unique_letters
+        
+
+
+def handle_client_request(file_content, connection):
+    print("handling client request...")
+    num_of_letters = count_letters_case_sensitive(file_content)
+    # time.sleep(20)
+    try:
+        connection.send(str.encode(num_of_letters))
+    except BrokenPipeError:
+        print("Client Disconnected\n")
+
+
+def main():
+    cmd_args = sys.argv
+    validated_args = handle_arguments(cmd_args)    
+    parsed_args = parse_arguments(validated_args)
+    start_server(parsed_args)
+main()
